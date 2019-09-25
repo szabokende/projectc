0ca66500aa9713c518d4dfb9b8ea53f5cb1d9bc7
KendeSzabo <szabo.kende20@gmail.com>
2019-09-25 13:45:46 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        double cost= 0;
+        int couponPercentage =0 ;
+        double discount = 0;
+
+    }
+}
